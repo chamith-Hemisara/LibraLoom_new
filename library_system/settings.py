6624686b062a68ee19826c1ab092af83dd520430
chamith-Hemisara <82514256+chamith-Hemisara@users.noreply.github.com>
2023-11-13 18:35:37 +0530

    Update settings.py

--- library_system/settings.py
+++ library_system/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     BASE_DIR / 'LibraLoom/static'
 ]
