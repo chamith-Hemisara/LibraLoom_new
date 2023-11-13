9f0c3a11922bc0cc3cef755d8c4c727607562a55
chamith-Hemisara <82514256+chamith-Hemisara@users.noreply.github.com>
2023-11-13 13:48:23 +0530

    Update settings.py

--- library_system/settings.py
+++ library_system/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'library_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.mysql',
-        'NAME':'defaultdb',
-        'USER':'doadmin',
-        'PASSWORD':'AVNS_z3xYuJd0Hfp-zd-D4ti',
-        'HOST':'librarysystem20-do-user-14977883-0.c.db.ondigitalocean.com',
-        'PORT':'25060',
+        'NAME':'library_system',
+        'USER':'root',
+        'PASSWORD':'chamith',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
